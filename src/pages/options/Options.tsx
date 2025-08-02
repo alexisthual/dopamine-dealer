e0e0b743a6a2399c86a8182a6f987d866949b66e
Alexis Thual <13835654+alexisthual@users.noreply.github.com>
2025-08-03 00:00:02 +0200

    Blank options

--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import "@pages/options/Options.css";
 
 export default function Options() {
-  return <div className="container">Options</div>;
+  return <div className="container"></div>;
 }
